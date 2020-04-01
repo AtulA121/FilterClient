e634988052c3ee26cdaec4b7350e372af211904d
a121 <shankar.pisal@codevian.com>
2020-04-01 11:06:32 +0530

    added some changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {HttpClient} from "@angular/common/http"
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  students : any;
+  filterStudent : any;
+  private _searchTerm : string;
+
   title = 'filterClient';
+
+  constructor(private _http : HttpClient){
+    this._http.get("http://localhost:3000/getData").subscribe((res : any)=>{
+      this.students=res.result;
+      this.filterStudent=this.students;
+    });
+  }
+
+  get searchTerm(){
+    return this._searchTerm;
+  }
+
+  set searchTerm(value){
+    this._searchTerm=value;
+    this.filterStudent=this.getFilterData(value);
+  }
+
+  getFilterData(value){
+    return this.students.filter((stu : any)=>{
+      return stu.name.toLowerCase().indexOf(value.toLowerCase())!==-1 ? true : stu.email.toLowerCase().indexOf(value.toLowerCase())!==-1 ? true : false;
+    });
+  }
 }
